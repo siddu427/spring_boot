0ce9f5abaf89c9f47d5eb9910c3caf33847e8752
siddarth <siddarthakamble427@gmail.com>
2019-03-28 23:37:34 +0530

    modified

--- src/com/ntest/Test.java
+++ src/com/ntest/Test.java
@@ -3,6 +3,9 @@ package com.ntest;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello jenkins");
+		if("siddu".equals("akmble")) {
+			System.out.println("siddartha");
+		}
 	}
 
 }
