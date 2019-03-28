662f3bea8caebdf6e168f6e002aca7627901cda9
siddu427 <44061745+siddu427@users.noreply.github.com>
2019-03-28 23:49:52 +0530

    test class if condition changed

--- src/com/ntest/Test.java
+++ src/com/ntest/Test.java
@@ -3,7 +3,7 @@ package com.ntest;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello jenkins");
-		if("siddu".equals("akmble")) {
+		if("siddu".equals("kamble")) {
 			System.out.println("siddartha");
 		}
 	}
